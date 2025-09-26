ebad1650701450b4d2ae856e440ef2ca277f0329
Dennis-ux-ai <dennissabu444@gmail.com>
2025-09-26 18:11:20 +0530

    feat(): Added Hero section ,Footer , Faq , andd added motion from framer and gsap

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import Navbar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Runnum from "./components/runnum"
+import Niga from "./components/niga"
+import Footer from "./components/Footer"
 
 const page = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+      <Runnum />
+      <Niga />
+      <Footer />
+      
+    </>
   )
 }
 
