f1b76996a7b8040d1326b9e39cdb115839daf408
Abin Antony <abina5448@gmail.com>
2025-09-27 09:48:02 +0530

    removed doctor- hospital

--- server/api/routers.ts
+++ server/api/routers.ts
@@ -1,14 +1,10 @@
 import { createTRPCRouter } from './trpc';
 import { authRouter } from '../routes/authRoute';
-import { hospitalRouter } from '../routes/hospitalRoute';
-import { doctorRouter } from '../routes/doctorRoute';
 import { user } from '../db/schema';
 import { userRouter } from '../routes/userRoute';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
-    hospital: hospitalRouter,
-    doctor: doctorRouter,
     user: userRouter,
 });
 
